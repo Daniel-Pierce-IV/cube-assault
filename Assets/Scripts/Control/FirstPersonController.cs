6ad905a363fe4a06c3e81a246c1e3ad196c077fe
Daniel Pierce IV <daniel.pierce.iv@gmail.com>
2019-10-30 23:06:48 -0400

    Created the controller and movement.

---
+++ Assets/Scripts/Control/FirstPersonController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cube.Control
+{
+	[RequireComponent(typeof(FirstPersonMovement))]
+	public class FirstPersonController : MonoBehaviour
+	{
+		private FirstPersonMovement firstPersonMovement;
+
+		// Start is called before the first frame update
+		void Start()
+		{
+			firstPersonMovement = GetComponent<FirstPersonMovement>();
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+			firstPersonMovement.HandleInput(GetMovementInput());
+		}
+
+		private Vector3 GetMovementInput()
+		{
+			// TODO playtest this version versus normalized version
+			return new Vector3(
+				Input.GetAxisRaw("Horizontal"),
+				0,
+				Input.GetAxisRaw("Vertical"));
+		}
+	}
+}
---
+++ Assets/Scripts/Control/FirstPersonMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cube.Control
+{
+	[RequireComponent(typeof(Rigidbody))]
+	[RequireComponent(typeof(CapsuleCollider))]
+	public class FirstPersonMovement : MonoBehaviour
+	{
+		[SerializeField] private float speed = 5f;
+		private Rigidbody rigidbody;
+
+		// Start is called before the first frame update
+		void Start()
+		{
+			rigidbody = GetComponent<Rigidbody>();
+		}
+
+		public void HandleInput(Vector3 movementInput)
+		{
+			// Using velocity instead of AddForce()
+			// for its instant start/stop without acceleration
+			rigidbody.velocity = CalculateNewRigidbodyVelocity(movementInput);
+		}
+
+		private Vector3 CalculateNewRigidbodyVelocity(Vector3 movementInput)
+		{
+			Vector3 velocity = movementInput * speed;
+			velocity.y = rigidbody.velocity.y;
+
+			return velocity;
+		}
+	}
+}
+
