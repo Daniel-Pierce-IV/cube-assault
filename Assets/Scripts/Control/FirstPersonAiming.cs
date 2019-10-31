f7aad194a5888f535483eca963f927dbb86e8ab8
Daniel Pierce IV <daniel.pierce.iv@gmail.com>
2019-10-31 15:22:32 -0400

    Created mouse aiming.

---
+++ Assets/Scripts/Control/FirstPersonAiming.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cube.Control
+{
+	public class FirstPersonAiming : MonoBehaviour
+	{
+		[SerializeField] private Camera camera;
+		[SerializeField] private float sensitivity = 200f;
+
+		private float curPitch = 0f;
+		private float maxPitch = 90f; // Straight down
+		private float minPitch = -90f; // Straight up
+
+		private const float UP_ROTATION_ANGLE = 270f;
+		private const float DOWN_ROTATION_ANGLE = 90f;
+
+		public void LockCursor()
+		{
+			Cursor.lockState = CursorLockMode.Locked;
+		}
+
+		public void HandleInput(Vector2 mouseInput)
+		{
+			UpdateYaw(mouseInput.x);
+			UpdatePitch(mouseInput.y);
+		}
+
+		private void UpdateYaw(float mouseInputX)
+		{
+			float yaw = mouseInputX * sensitivity * Time.deltaTime;
+			transform.Rotate(0, yaw, 0);
+		}
+
+		private void UpdatePitch(float mouseInputY)
+		{
+			// Invert mouse y for intuitive mouse aiming
+			float pitch = -mouseInputY * sensitivity * Time.deltaTime;
+			curPitch += pitch;
+
+			// Custom clamp for Unity rotation angles
+			if(curPitch > maxPitch || curPitch < minPitch)
+			{
+				if(curPitch > maxPitch)
+				{
+					curPitch = maxPitch;
+					SetCameraEulerPitch(DOWN_ROTATION_ANGLE);
+				}
+				else
+				{
+					curPitch = minPitch;
+					SetCameraEulerPitch(UP_ROTATION_ANGLE);
+				}
+			}
+			else
+			{
+				camera.transform.Rotate(pitch, 0, 0);
+			}
+		}
+
+		private void SetCameraEulerPitch(float clampAngle)
+		{
+			Vector3 eulerRotation = camera.transform.eulerAngles;
+			eulerRotation.x = clampAngle;
+			camera.transform.eulerAngles = eulerRotation;
+		}
+	}
+}
--- Assets/Scripts/Control/FirstPersonController.cs
+++ Assets/Scripts/Control/FirstPersonController.cs
@@ -6,20 +6,27 @@ using UnityEngine;
 namespace Cube.Control
 {
 	[RequireComponent(typeof(FirstPersonMovement))]
+	[RequireComponent(typeof(FirstPersonAiming))]
 	public class FirstPersonController : MonoBehaviour
 	{
 		private FirstPersonMovement firstPersonMovement;
+		private FirstPersonAiming firstPersonAiming;
 
 		// Start is called before the first frame update
 		void Start()
 		{
 			firstPersonMovement = GetComponent<FirstPersonMovement>();
+			firstPersonAiming = GetComponent<FirstPersonAiming>();
+
+			firstPersonAiming.LockCursor();
 		}
 
 		// Update is called once per frame
 		void Update()
 		{
+			// Handle movement before handling the camera to avoid stuttering
 			firstPersonMovement.HandleInput(GetMovementInput());
+			firstPersonAiming.HandleInput(GetMouseInput());
 		}
 
 		private Vector3 GetMovementInput()
@@ -29,6 +36,13 @@ namespace Cube.Control
 				Input.GetAxisRaw("Horizontal"),
 				0,
 				Input.GetAxisRaw("Vertical"));
+		}
+
+		private Vector2 GetMouseInput()
+		{
+			return new Vector2(
+				Input.GetAxis("Mouse X"),
+				Input.GetAxis("Mouse Y"));
 		}
 	}
 }
