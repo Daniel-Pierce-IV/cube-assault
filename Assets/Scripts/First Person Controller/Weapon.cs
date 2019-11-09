36c7454577b3f97f339597304b99ce0e41ae69c0
Daniel Pierce IV <daniel.pierce.iv@gmail.com>
2019-11-09 18:32:37 -0500

    Player weapons changed from particle systems to instantiated game objects.

--- Assets/Scripts/First Person Controller/Weapon.cs
+++ Assets/Scripts/First Person Controller/Weapon.cs
@@ -7,56 +7,87 @@ namespace PlayerController
 	public class Weapon : MonoBehaviour
 	{
 		[SerializeField] private float fireRate = 0.5f;
+		[SerializeField] private float bulletSpeed = 50f;
 
-		private ParticleSystem _particleSystem;
-		private List<ParticleCollisionEvent> _collisionEvents;
-		private bool _isActive = false;
-		private float _cooldownTimestamp;
+		[Tooltip("Number of bullets shot per trigger pull.")]
+		[SerializeField] private int bulletCount = 1;
 
-		private void Start()
-		{
-			_particleSystem = GetComponent<ParticleSystem>();
-			_collisionEvents = new List<ParticleCollisionEvent>();
-		}
+		[Range(0f, 1f)]
+		[SerializeField] private float bulletSpreadIntensity = 0f;
+
+		[SerializeField] private GameObject bulletPrefab;
+		[SerializeField] private GameObject spawnZone;
+
+		private bool _isFiring = false;
+		private bool _isOnCooldown = false;
 
 		public void Activate()
 		{
-			if (CanActivate())
-			{
-				_isActive = true;
-				_cooldownTimestamp = Time.time + fireRate;
-				_particleSystem.Play();
-			}
+			_isFiring = true;
+			Fire();
 		}
 
 		public void Deactivate()
 		{
-			_isActive = false;
-			_particleSystem.Stop();
+			_isFiring = false;
 		}
 
 		public bool IsActive()
 		{
-			return _isActive;
+			return _isFiring;
 		}
 
-		public bool CanActivate()
+		private void Fire()
 		{
-			return Time.time >= _cooldownTimestamp;
-		}
-
-		private void OnParticleCollision(GameObject other)
-		{
-			_particleSystem.GetCollisionEvents(other, _collisionEvents);
-
-			foreach (ParticleCollisionEvent collisionEvent in _collisionEvents)
+			if (!_isOnCooldown)
 			{
-				GameObject hitObject = collisionEvent.colliderComponent.gameObject;
-				if (LayerMask.LayerToName(hitObject.layer) == "Hit Box")
+				for (int i = 0; i < bulletCount; i++)
 				{
-					Destroy(other);
+					CreateBullet();
 				}
+				StartCoroutine(Cooldown());
 			}
+		}
+
+		private void CreateBullet()
+		{
+			GameObject bullet = Instantiate(
+				bulletPrefab,
+				spawnZone.transform.position,
+				spawnZone.transform.rotation);
+
+			Vector3 spawnPoint = GetRandomLocalPointInZone();
+
+			// Move the bullet to a random point in the spawn zone
+			bullet.transform.Translate(spawnPoint);
+
+			// Rotate the bullet left/right depending on
+			// the distance it is from the center of the spawn zone
+			Vector3 bulletDir = bullet.transform.position - transform.position;
+			float angle = Vector3.Angle(bulletDir, transform.forward) * bulletSpreadIntensity;
+			bullet.transform.Rotate(Vector3.up * Mathf.Sign(spawnPoint.x) * angle);
+
+			bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * bulletSpeed;
+		}
+
+		private Vector3 GetRandomLocalPointInZone()
+		{
+			BoxCollider boxCollider = spawnZone.GetComponent<BoxCollider>();
+			float areaHalfHeight = boxCollider.size.y / 2;
+			float areaHalfWidth = boxCollider.size.x / 2;
+
+			// NOTE: Ignore the z-depth, don't want any depth variation
+			return new Vector3(
+				Random.Range(-areaHalfWidth, areaHalfWidth),
+				Random.Range(-areaHalfHeight, areaHalfHeight),
+				0);
+		}
+
+		IEnumerator Cooldown()
+		{
+			_isOnCooldown = true;
+			yield return new WaitForSeconds(fireRate);
+			_isOnCooldown = false;
 		}
 	}
 }
