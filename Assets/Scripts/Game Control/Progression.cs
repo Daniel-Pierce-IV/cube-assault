5230299b2dfea0d89e4dda5e770e1a0afa2d9622
Daniel Pierce IV <daniel.pierce.iv@gmail.com>
2019-11-11 00:09:52 -0500

    Implement progression tracker, for other objects to measure their progress by.

---
+++ Assets/Scripts/Game Control/Progression.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Progression : MonoBehaviour
+{
+	[Tooltip("Length of time (in seconds) before there is no more progression.")]
+	[SerializeField] private float duration = 300f;
+
+	public float Percent()
+	{
+		return Mathf.Clamp01(Time.timeSinceLevelLoad / duration);
+	}
+}
---
+++ Assets/Scripts/IProgressive.cs
@@ -0,0 +1,4 @@
+﻿interface IProgressive
+{
+	void Progress();
+}
